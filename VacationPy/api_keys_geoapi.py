b08e592274a2572df6828452c4705d57f079af42
majirajib4747 <43809492+majirajib4747@users.noreply.github.com>
2023-07-24 22:47:00 -0400

    1st commit Python API Challenge

---
+++ VacationPy/api_keys_geoapi.py
@@ -0,0 +1,2 @@
+# Geoapify API Key
+geoapify_key = "a2fb98e175d5468b87566da2441a6f7e"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "38f6ef382b0496729f91ca94b90aa196"
+
+# Geoapify API Key
+geoapify_key = "a2fb98e175d5468b87566da2441a6f7e"
